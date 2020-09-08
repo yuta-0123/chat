ca06257ba794d3fdd79fe2c7ad151994081eb0af
yuta-0123 <yuta.fukuda.0913@gmail.com>
2020-09-08 12:36:07 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="container relative min-h-screen pt-16">
+    <Header />
+    <nuxt />
+  </main>
+</template>
+
+<script>
+import Header from '~/components/Header'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
